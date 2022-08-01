3a09825a58cfcbfb941933241c9828e593c336e4
Zeynep Ceyhan <104215922+zeynepnceyhan@users.noreply.github.com>
2022-08-01 16:12:52 +0300

    sum of two numbers
    internship

---
+++ SumOfNumbers/SumOfNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2, sum;
+            Console.WriteLine("First number: ");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Second number: ");
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+            sum = number1 + number2;
+            Console.WriteLine("The sum of these two numbers: " + sum);
+            Console.ReadLine();
+        }
+
+    }
+
+}
